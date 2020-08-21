b66a72599c5b75be35c8bd3a98386b32791ad576
esther-soyoung <esther.soyoung@gmail.com>
2020-08-21 16:44:06 +0900

    consecutive executions

--- codes/ny.sh
+++ codes/ny.sh
@@ -9,14 +9,14 @@ python main.py \
 	--data_path $DATADIR \
 	--save_path './ny/l2_001/' \
 	--pretrain $PRETRAIN \
-	--L2 0.001 &
+	--L2 0.001 &&
 
 python main.py \
 	--data_name $DATA \
 	--data_path $DATADIR \
 	--save_path './ny/l2_0001/' \
 	--pretrain $PRETRAIN \
-	--L2 0.0001 &
+	--L2 0.0001 &&
 
 # learning rate
 python main.py \
@@ -24,14 +24,14 @@ python main.py \
 	--data_path $DATADIR \
 	--save_path './ny/lr_00005/' \
 	--pretrain $PRETRAIN \
-	--learning_rate 0.00005 &
+	--learning_rate 0.00005 &&
 
 python main.py \
 	--data_name $DATA \
 	--data_path $DATADIR \
 	--save_path './ny/lr_001/' \
 	--pretrain $PRETRAIN \
-	--learning_rate 0.001 &
+	--learning_rate 0.001 &&
 
 # lr step
 python main.py \
@@ -39,7 +39,7 @@ python main.py \
 	--data_path $DATADIR \
 	--save_path './ny/lrstep_3/' \
 	--pretrain $PRETRAIN \
-	--lr_step 3 &
+	--lr_step 3 &&
 
 # lr decay
 python main.py \
@@ -47,7 +47,7 @@ python main.py \
 	--data_path $DATADIR \
 	--save_path './ny/lrdecay_0.05/' \
 	--pretrain $PRETRAIN \
-	--lr_decay 0.05 &
+	--lr_decay 0.05 &&
 
 python main.py \
 	--data_name $DATA \
