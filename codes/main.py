9caaa81bea69d5c15165d9c55e7c5b3e5805a2ed
esther-soyoung <esther.soyoung@gmail.com>
2020-08-21 16:39:42 +0900

    mkdir save_path

--- codes/main.py
+++ codes/main.py
@@ -111,7 +111,7 @@ def run(args):
                                                        len([y for x in test_idx for y in test_idx[x]])))
     SAVE_PATH = args.save_path
     tmp_path = 'checkpoint/'
-    os.mkdirs(SAVE_PATH + tmp_path)
+    os.makedirs(SAVE_PATH + tmp_path)
 
     # writer = SummaryWriter(args.data_name)
     for epoch in range(parameters.epoch):
@@ -279,7 +279,7 @@ if __name__ == '__main__':
     if args.pretrain == 1:
         args = load_pretrained_model(args)
 
-    os.mkdirs(args.save_path)
+    os.makedirs(args.save_path)
     logger = get_logger(logpath=os.path.join(args.save_path, 'logs'), filepath=os.path.abspath(__file__))
     logger.info(args)
 
